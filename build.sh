Subhaam/kernel_lenovo_msm8953-1

script: misc: Add simple build script
Signed-off-by: FireLord1 <firelord.xda@gmail.com>

build.sh
@@ -1,0 +1,5 @@
+export ARCH=arm64
+export CROSS_COMPILE=~/android/kernel/toolchain/google-64-4.9/bin/aarch64-linux-android-
+make mrproper
+make p2a42_defconfig
+make -j10

FireLord/android_kernel_lenovo_msm8953

Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

build.sh
@@ -1,0 +1,5 @@
+export ARCH=arm64
+export CROSS_COMPILE=~/android/kernel/toolchain/google-64-4.9/bin/aarch64-linux-android-
+make mrproper
+make kuntao_defconfig
+make -j10